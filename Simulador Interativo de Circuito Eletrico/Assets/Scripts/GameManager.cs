a042a486445075aa2883c8692cbcaec9ab5fa553
Eduardo Seitz <eduardoseitz@outlook.com.br>
2025-02-16 18:55:36 -0300

    Adicionado menu principal

--- Simulador Interativo de Circuito Eletrico/Assets/Scripts/GameManager.cs
+++ Simulador Interativo de Circuito Eletrico/Assets/Scripts/GameManager.cs
@@ -3,31 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    
-    #region Declaracoes
-
-    public static GameManager instance;
-
-    #endregion
-
-    #region Funcoes MonoBehaviour
-
-    private void Awake()
-    {
-        // Padrao singleton.
-        if (instance)
-            Destroy(this);
-        else
-            instance = this;
-    }
-
-    #endregion
-
     #region Funcoes Ajudantes
 
     public void RestartLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        LoadLevel(SceneManager.GetActiveScene().buildIndex);
+    }
+    
+    public void LoadLevel(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
     }
 
     #endregion
